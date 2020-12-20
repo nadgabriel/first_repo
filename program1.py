6b0ae206e5a99920f9c10b89768805b58f009b9c
GabrielNad <nadgabriell@gmail.com>
2020-12-20 22:04:13 +0100

    modify program1 variables

--- program1.py
+++ program1.py
@@ -1,8 +1,30 @@
-# My python program
+# Do premennej mozme ulozit hocijaky datovy typ
+pocet_jablk=2 
+pocet_hrusiek=3.4
 
+number_of_windows=10
 
-if __name__ == '__main__':
-    main()
-    
-    main()
-        pass    
+print("pocet_jabl = ", pocet_jablk)
+
+# Premenna je case sensitive cize tieto dve premenne su rozdielne
+cars=1
+Cars=2
+
+# Premennu mozme definovat aj niekolko krat
+jablka=4
+jablka=3
+
+# Nazvy premennych mozu pozostavat len z malych a velkych pismen anglickej abecedy a podtrzniku _
+toto_je_premenna = 4
+
+# Vzdy pouzivajte nazvy ktore popisuju obsah premennej
+pocet_byvalych_frajeriek = 12
+
+# Medzi rovnasa premenou a cislom moze byt lubovolny pocet medzier. Ja odporucam nedavat ziadnu
+pocet_zubov=32
+pocet_zubov = 32
+pocet_zubov     =      32
+
+print("pocet_zubov= ", pocet_zubov) 
+# Miso odporuca pouzivat anglicke nazvy premennych. Ked budete programovat pre firmy kazdy bude pouzivat anglicke nazvy
+number_of_limbs=2
